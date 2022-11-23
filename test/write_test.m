63f256cc18372dd5b97c6c155fa4ed439258b824
meili <gymmyp1@gmail.com>
2022-11-23 11:09:46 -0500

    running cp_als tests

--- test/write_test.m
+++ test/write_test.m
@@ -7,10 +7,14 @@ tic
 
 fprintf("Saving tensor to file...\n");
 %t = read_htns('uber_trim.txt');
-t = read_htns('uber.txt');
+%t = read_htns('uber.txt');
+%t = read_htns('chicago.txt');
+t = read_htns('enron.txt');
 
 %matfile = "ubertrim_hacoo.mat";
-matfile = "uber_hacoo.mat";
+%matfile = "uber_hacoo.mat";
+%matfile = "chicago_hacoo.mat";
+matfile = "enron_hacoo.mat";
 write_htns(t,matfile);
 
 toc
