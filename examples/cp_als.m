eb6685537d43720989db394d5a5a5578c307f2d7
meili <gymmyp1@gmail.com>
2023-08-01 10:34:50 -0400

    prep for merge to main for journal submision

--- examples/cp_als.m
+++ examples/cp_als.m
@@ -2,15 +2,11 @@
     Compute CP-ALS.
 %}
 
-tic
 %create the HaCOO tensor
-T = read_htns("uber.txt")
-toc
+T = read_htns("uber.txt") %tensor can be downloaded from the FROSTT repository
 
 %specify number of components
 R = 50;
 
-tic
-%Computes an estimate of the best rank-R CP model of a tensor X (requires Tensor Toolbox)
+%Compute an estimate of the best rank-R CP model of a tensor X (requires Tensor Toolbox)
 V = htns_cp_als(T,R);
-toc
